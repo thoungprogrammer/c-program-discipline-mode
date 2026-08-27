a6d26426d2a95cf79a8544625f97310016018eb6
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-08-27 07:50:37 +0700

    solving challenge 2

---
+++ section_6/challenge_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+// PROGRAM TO SHOW FULL NAME, AGE AND GENDER
+
+int main(){
+	printf("Full Name: Morn Chanthoung\n");
+	printf("Age: 20 Years Old\n");
+	printf("Gender: Male\n");
+	return 0;
+}
