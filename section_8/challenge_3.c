e95743ce0113cf6d6c913da9f0378d7b18cc8de0
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-08-28 08:00:49 +0700

    solve challenge 3 in section 8

---
+++ section_8/challenge_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// PRINT X SHAPE ASTERISK 
+
+int main(){
+    printf(" *     * \n");
+    printf("  *   *  \n");
+    printf("   * *   \n");
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  *   *  \n");
+    printf(" *     * \n");
+    return 0;
+}
