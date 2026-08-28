1bb39e2d6704df34f2357371fcbc2bb7b63541ee
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-08-28 08:53:54 +0700

    complete soving challenge 4 in section 8

--- section_8/challenge_4.c
+++ section_8/challenge_4.c
@@ -3,7 +3,8 @@
 // PRINT THE YEAR WE WERE BORN 
 
 int main(){
-    printf("Current year: %d\n");
-    printf("La")
+    printf("Current year: %d\n",2026);
+    printf("Current age: %d\n", 20);
+    printf("The year that you was born is %d\n",2026-20);
     return 0;
 }
