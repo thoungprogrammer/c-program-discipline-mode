fd77e3200175cf618321128a8cbe914f22247088
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-08-28 07:55:23 +0700

    solve challenge 2 in section 8

---
+++ section_8/challenge_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// PRINT THE FOLLOWING PATTERN: 
+// 1********* 
+// 12********
+// 123*******
+// 124*******
+// 1245******
+
+int main(){
+    printf("1*********\n");
+    printf("12********\n");
+    printf("123*******\n");
+    printf("1234******\n");
+    printf("12345*****\n");
+    return 0;
+}
