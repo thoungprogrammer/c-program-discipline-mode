e1b9790af15d84c79c9e9b51b16b3c1b9d89dc57
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-08-29 12:16:43 +0700

    solve challenge 1 in section 9

---
+++ section_9/challenge_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+// PROGRAM TO CALCULATE AND PRINT THE YEAR YOU WERE BORN
+int main(){
+    int currentYear;
+    int yourAge;
+    int bornYear;
+
+    printf("Please enter your current year: \n> ");
+    scanf("%d", &currentYear);
+    printf("Please enter your current age: \n> ");
+    scanf("%d", &yourAge);
+    bornYear = currentYear - yourAge;
+    printf("The year that your were born is %d.\n", bornYear);
+    return 0;
+}
