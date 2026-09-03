08a102478108036d02a6e39491a576551a5cf1b4
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-09-03 23:47:28 +0700

    solve challenge 3 in section 13 but not complete so I'll complete tomorrow

---
+++ section_13/challenge_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// program to find maximum and minimum number between three number
+
+int main(){
+    int firstNum;
+    int secondNum;
+    int thirdNum;
+    printf("Enter the first number: ");
+    scanf("%i",&firstNum);
+    printf("Enter the second number: ");
+    scanf("%i",&secondNum);
+    printf("Enter the third number: ");
+    scanf("%i",&thirdNum);
+    
+    return 0;
+}
