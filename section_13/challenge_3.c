3838b6699867f0d6f2e2815548c351443030b7ad
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-09-05 04:37:43 -0400

    solve the challenge 3 in section 13 is there something wrong I will come back and fix my code in the future

--- section_13/challenge_3.c
+++ section_13/challenge_3.c
@@ -1,17 +1,40 @@
 #include<stdio.h>
 
-// program to find maximum and minimum number between three number
+// program to find the maximum number and minimum number between three input ( numbers )
 
 int main(){
     int firstNum;
     int secondNum;
     int thirdNum;
-    printf("Enter the first number: ");
-    scanf("%i",&firstNum);
-    printf("Enter the second number: ");
-    scanf("%i",&secondNum);
-    printf("Enter the third number: ");
-    scanf("%i",&thirdNum);
-    
+    printf("Enter the first integer number: ");
+    scanf("%d",&firstNum);
+    printf("Enter the second integer number: ");
+    scanf("%d",&secondNum);
+    printf("Enter the third integer number: ");
+    scanf("%d",&thirdNum);
+    if ( firstNum > secondNum ){
+        if( secondNum > thirdNum ){
+            printf("The maximum number is %d and the minimum number is %d\n",firstNum,thirdNum);
+        }
+        else{
+            printf("The maximum number is %d and the minimum number is %d\n", firstNum,secondNum);
+        }
+    }
+    else if ( secondNum > thirdNum){
+        if ( thirdNum > firstNum){
+            printf("The maximum number is %d and the minimum number is %d\n",secondNum,firstNum);
+        }
+        else{
+            printf("The maximum number is %d and the minimum number is %d\n",secondNum,thirdNum);
+        }
+    }
+    else if ( thirdNum > firstNum){
+        if ( firstNum > secondNum){
+            printf("The maximum number is %d and the minimum number is %d\n",thirdNum,secondNum);
+        }
+        else{
+            printf("The maximum number is %d and the minimum number is %d\n", thirdNum,firstNum);
+        }
+    }
     return 0;
 }
